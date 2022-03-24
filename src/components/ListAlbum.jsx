7e6e71c4cdb927cf54ccbd3d48b30843eb72dff5
Guilherme R <grsguiga@gmail.com>
2022-03-24 17:49:32 -0300

    Requisito 6

---
+++ src/components/ListAlbum.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ListAlbum extends Component {
+  sendIdAlbum = (id) => {
+    localStorage.setItem('idAlbum', { id });
+  }
+
+  render() {
+    const { result } = this.props;
+    return (
+      <div>
+        {
+          result.length === 0
+            ? (
+              <p>
+                Nenhum álbum foi encontrado
+              </p>
+            )
+            : (
+              <ul>
+                {
+                  result.map(({ artistName,
+                    collectionName,
+                    artworkUrl100,
+                    collectionId },
+                  index) => (
+                    <li key={ index }>
+                      <img src={ artworkUrl100 } alt={ collectionName } />
+                      <br />
+                      {`Artista: ${artistName}`}
+                      <br />
+                      {`Album: ${collectionName}`}
+                      <br />
+                      <Link
+                        data-testid={ `link-to-album-${collectionId}` }
+                        to={ `/album/${collectionId}` }
+                        onClick={ this.sendIdAlbum }
+                      >
+                        More Info
+                      </Link>
+                    </li>
+                  ))
+                }
+              </ul>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+ListAlbum.propTypes = {
+  result: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default ListAlbum;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,56 +1,109 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import ListAlbum from '../components/ListAlbum';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.state = {
-      listenInput: '',
+      input: '',
+      name: null,
       disabled: true,
+      loading: false,
+      result: null,
+      showArtistName: false,
     };
   }
 
   listenInput = ({ target }) => {
     const minLetters = 2;
     this.setState({
-      listenInput: target.value,
+      input: target.value,
     }, () => {
-      const { listenInput } = this.state;
-      const result = listenInput.length >= minLetters;
+      const { input } = this.state;
+      const result = input.length >= minLetters;
       this.setState({
         disabled: !result,
       });
     });
   }
 
+  artistSearch = async () => {
+    const { input } = this.state;
+    this.showArtistName();
+    this.setState({ loading: true, name: input, input: '' });
+    const result = await searchAlbumsAPI(input);
+    this.setState({
+      result,
+    }, () => {
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
+  showArtistName = () => {
+    this.setState({
+      showArtistName: true,
+    });
+  }
+
   render() {
-    const { disabled } = this.state;
+    const { disabled, input, loading, result, showArtistName, name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <div>
-          <form>
-            <label htmlFor="search-artist-input">
-              <input
-                type="text"
-                data-testid="search-artist-input"
-                placeholder="Nome do Artista"
-                id="search-artist-input"
-                onChange={ this.listenInput }
-              />
-            </label>
-            <label htmlFor="search-artist-button">
-              <input
-                type="button"
-                data-testid="search-artist-button"
-                id="search-artist-button"
-                value="Pesquisar"
-                disabled={ disabled }
-              />
-            </label>
-          </form>
-        </div>
+        {
+          loading
+            ? <Loading />
+            : (
+              <div>
+                <form>
+                  <label htmlFor="search-artist-input">
+                    <input
+                      type="text"
+                      data-testid="search-artist-input"
+                      placeholder="Nome do Artista"
+                      id="search-artist-input"
+                      onChange={ this.listenInput }
+                      value={ input }
+                    />
+                  </label>
+                  <label htmlFor="search-artist-button">
+                    <input
+                      type="button"
+                      data-testid="search-artist-button"
+                      id="search-artist-button"
+                      value="Pesquisar"
+                      disabled={ disabled }
+                      onClick={ this.artistSearch }
+                    />
+                  </label>
+                </form>
+              </div>
+            )
+        }
+        {
+          showArtistName
+            ? (
+              <p>
+                {`Resultado de álbuns de: ${name}`}
+              </p>
+            )
+            : null
+        }
+        {
+          result === null
+            ? null
+            : (
+              <div>
+                <ListAlbum result={ result } />
+              </div>
+            )
+        }
       </div>
     );
   }
