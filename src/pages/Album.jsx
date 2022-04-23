39976ed081f142a9fa4186c1f0a17849d319015f
Guilherme R <grsguiga@gmail.com>
2022-04-23 16:33:32 -0300

    Add fav Icon

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -101,11 +101,11 @@ class Album extends Component {
                             data-testid={ `checkbox-music-${song.trackId}` }
                             type="checkbox"
                             name="fav"
-                            id="fav"
+                            id={`fav-${index}`}
                             onChange={ (event) => this.handleInput(event, song) }
                             checked={ this.isChecked(song) }
                           />
-                          <label htmlFor="fav">
+                          <label htmlFor={`fav-${index}`}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#000000">
                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                             </svg>
